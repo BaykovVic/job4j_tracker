0d6a854851a3caafa02970d8b4d1ace1d8340bcc
victor <vic.baykov89@gmail.com>
2023-08-02 11:11:10 +0300

    1.5 Поля объекта [#166083]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,32 @@
+package ru.job4j.oop;
+
+public class Cat {
+    private String name;
+    private String food;
+
+    public void show() {
+        System.out.println("My name is " + this.name);
+        System.out.println("I ate " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black");
+        black.eat("fish");
+        black.show();
+    }
+}
