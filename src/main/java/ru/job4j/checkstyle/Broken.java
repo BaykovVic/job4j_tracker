2da6d23ad230990bc7fc9a78b5cfd0a993e646e7
victor <vic.baykov89@gmail.com>
2023-08-02 10:37:38 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    public static final String NEW_VALUE = "";
+    public String surname;
+    private int sizeOfEmpty = 10;
+
+    private String name;
+
+    Broken() {
+    }
+
+    private void echo() { }
+
+    private void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    private void method(int a, int b, int c, int d, int e, int f, int g) {
+    }
+
+}
